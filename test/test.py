52f7bce2b892265552397cc6b6db29cdb3c24c5f
someoneinjd <someoneinjd@outlook.com>
2024-12-06 21:07:49 +0800

    Fix result compare

--- test/test.py
+++ test/test.py
@@ -1,93 +1,92 @@
 #!/usr/bin/env python3
 import subprocess
 import os
+import sys
 import argparse
 
-IRBuild_ptn = "{} -emit-ir {} -o {} {}"
-ExeGen_ptn = '"clang" "-O0" "-o" "{}" "{}" "../lib/lib.c"'
-Exe_ptn = '"{}"'
+def eprint(*objects: object, sep: str = " ", end: str = "\n", flush: bool = False):
+    print(*objects, sep=sep, end=end, file=sys.stderr, flush=flush)
 
 
-def eval(EXE_PATH: str, TEST_BASE_PATH: str, opts: str, testcases: dict[str, bool]):
-    print("===========TEST START===========")
-    print("now in {}".format(TEST_BASE_PATH))
+def run_command(cmd: str, input=None):
+    return subprocess.run(
+        cmd, shell=True, capture_output=True, input=input, text=True
+    )
+
+
+def task_eval(
+    exe_path: str,
+    test_base_path: str,
+    testcases: dict[str, bool],
+    opts: str,
+    timeout: int,
+):
+    eprint("===========TEST START===========")
+    eprint(f"now in {test_base_path}")
     dir_succ = True
+    succ_count = 0
     max_len = max(len(case) for case in testcases)
     for case, need_input in testcases.items():
-        print(f"Case \x1b[1m{case:<{max_len}}\x1b[0m", end="")
-        TEST_PATH = os.path.join(TEST_BASE_PATH, case)
-        SY_PATH = os.path.join(TEST_BASE_PATH, case + ".sy")
-        LL_PATH = os.path.join(TEST_BASE_PATH, case + ".ll")
-        INPUT_PATH = os.path.join(TEST_BASE_PATH, case + ".in")
-        OUTPUT_PATH = os.path.join(TEST_BASE_PATH, case + ".out")
+        eprint(f"Case \x1b[1m{case:<{max_len}}\x1b[0m", end="")
+        test_path = os.path.join(test_base_path, case)
+        sy_path = test_path + ".sy"
+        ll_path = test_path + ".ll"
+        input_path = test_path + ".in"
+        output_path = test_path + ".out"
 
-        IRBuild_result = subprocess.run(
-            IRBuild_ptn.format(EXE_PATH, opts, LL_PATH, SY_PATH),
-            shell=True,
-            stderr=subprocess.PIPE,
-        )
-        if IRBuild_result.returncode == 0:
+        result = run_command(f"{exe_path} {opts} -emit-ir -o {ll_path} {sy_path}")
+        if result.returncode == 0:
             input_option = None
             if need_input:
-                with open(INPUT_PATH, "rb") as fin:
+                with open(input_path) as fin:
                     input_option = fin.read()
-
             try:
-                subprocess.run(
-                    ExeGen_ptn.format(TEST_PATH, LL_PATH),
-                    shell=True,
-                    stderr=subprocess.PIPE,
+                result = run_command(
+                    f"clang -O0 -o {test_path} {ll_path} ../lib/lib.c"
                 )
-                result = subprocess.run(
-                    Exe_ptn.format(TEST_PATH),
-                    shell=True,
-                    input=input_option,
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
-                )
-                out = result.stdout.split(b"\n")
-                if result.returncode != b"":
-                    out.append(str(result.returncode).encode())
-                for i in range(len(out) - 1, -1, -1):
-                    out[i] = out[i].strip(b"\r")
-                    if out[i] == b"":
-                        out.remove(b"")
-                case_succ = True
-                with open(OUTPUT_PATH, "rb") as fout:
-                    i = 0
-                    for line in fout.readlines():
-                        line = line.strip(b"\r").strip(b"\n")
-                        if line == "":
-                            continue
-                        if out[i] != line:
-                            dir_succ = False
-                            case_succ = False
-                        i = i + 1
-                    if case_succ:
-                        print("\t\033[32mPass\033[0m")
-                    else:
-                        print("\n".join(list(map(lambda x: x.decode(), out))))
-                        print("\t\033[31mWrong Answer\033[0m")
-            except Exception as _:
+                if result.returncode != 0:
+                    dir_succ = False
+                    eprint("\t\x1b[1;31mCompile Error\x1b[0m")
+                    continue
+                # Use the timeout command instead of subprocess's timeout option,
+                # as the latter has too many bugs in its implementation.
+                result = run_command(f"timeout {timeout} {test_path}", input=input_option)
+                if result.returncode == 124: # timeout
+                    dir_succ = False
+                    eprint("\t\x1b[1;31mTime Out\x1b[0m")
+                    continue
+                outs = result.stdout.splitlines()
+                outs.append(str(result.returncode))
+                outs = [line for line in outs if line]  # Remove empty line
+                answer = [
+                    line for line in open(output_path).read().splitlines() if line
+                ]
+                case_succ = outs == answer
+                dir_succ = dir_succ and case_succ
+                if case_succ:
+                    eprint("\t\x1b[1;32mPass\x1b[0m")
+                    succ_count += 1
+                else:
+                    eprint("\t\x1b[1;31mWrong Answer\x1b[0m")
+            except Exception as e:
                 dir_succ = False
-                print(_, end="")
-                print("\t\033[31mCodeGen or CodeExecute Fail\033[0m")
+                eprint(e, end="")
+                eprint("\t\x1b[31mCodeGen or CodeExecute Fail\x1b[0m")
             finally:
-                subprocess.call(["rm", "-rf", TEST_PATH, TEST_PATH])
-                subprocess.call(["rm", "-rf", TEST_PATH, TEST_PATH + ".o"])
-                subprocess.call(["rm", "-rf", TEST_PATH, TEST_PATH + ".ll"])
-
+                _ = run_command(f"rm -rf {test_path} {test_path}.o {test_path}.ll")
         else:
             dir_succ = False
-            print("\t\033[31mIRBuild Fail\033[0m")
-            subprocess.call(["rm", "-rf", TEST_PATH, TEST_PATH + ".ll"])
+            print(result)
+            eprint("\t\x1b[1;31mIRBuild Fail\x1b[0m")
+    eprint(
+        f"\t\x1b[1m{succ_count} Passed / {len(testcases) - succ_count} Failed\x1b[0m"
+    )
     if dir_succ:
-        print("\t\033[32mSuccess\033[0m in dir {}".format(TEST_BASE_PATH))
+        eprint(f"\t\x1b[1;32mSuccess\x1b[0m in dir {test_base_path}")
     else:
-        print("\t\033[31mFail\033[0m in dir {}".format(TEST_BASE_PATH))
+        eprint(f"\t\x1b[1;31mFail\x1b[0m in dir {test_base_path}")
 
-    print("============TEST END============")
-
+    eprint("============TEST END============")
 
 if __name__ == "__main__":
     # you can only modify this to add your testcase
@@ -133,4 +132,4 @@ if __name__ == "__main__":
             testcases[testcase_list[i][0]] = testcases[testcase_list[i][0]] | (
                 testcase_list[i][1] == "in"
             )
-        eval(EXE_PATH, TEST_BASE_PATH, " ".join(opts), testcases)
+        task_eval(EXE_PATH, TEST_BASE_PATH, testcases, " ".join(opts), 30)
